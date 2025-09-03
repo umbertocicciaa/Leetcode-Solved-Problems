29df02c10afaa10e900f3947d94788beff806565
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2025-09-03 10:04:54 +0200

    Time: 7 ms, Memory: 21.6 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def reverseString(self, s):
-        slen=len(s)
-        for i in range(slen//2):
-            if s[i]!=s[slen-1-i]:
-                tmp=s[slen-1-i]
-                s[slen-1-i]=s[i]
-                s[i]=tmp
-        
+        stack = []
+        for c in s:
+            stack.append(c)
+        for i in range(len(s)):
+            s[i]=stack.pop()
+            
